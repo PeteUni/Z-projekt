becb24950b12e1dfa3587aa03d731f90d2a6ce36
Pete Uni <peter.r.uni@op.pl>
2017-03-24 01:34:10 +0100

    pro2
    #

--- js/app.js
+++ js/app.js
@@ -1,37 +1,39 @@
 $(function() {
-//header
+    //header
 
 
 
 
 
-//section 1
+    //section 1
     var menu = $('.menu').find('li');
 
     menu.on('click', function() {
-        // $(this).toggleClass('blue');
+        $(this).toggleClass('blue');
         // console.log($(this).html());
     });
 
     var circle = $('.circle');
     var four = $('.four');
 
+    four.removeClass('puls');
     circle.on('click', function() {
-        four.animate({
-            opacity: 0.2,
-            transform: scale(0.3)
-            //transform: "rotate("+230+"deg)"
-            // transform: scale(0.5, 0.5)
-            // 'rotate(' + 180 + 'deg)'
-            // ,'linear', function() {
-            //     four.animate({
-            //         transform: 'rotate(' + 60 + 'deg)'
-        }, 1000);
+        four.addClass('puls');
+        // four.animate({
+        //     opacity: 0.05,
+        //     //transform: "rotate("+230+"deg)"
+        //     // transform: scale(0.5, 0.5)
+        //     // 'rotate(' + 180 + 'deg)'
+        //     // ,'linear', function() {
+        //     //     four.animate({
+        //     //         transform: 'rotate(' + 60 + 'deg)'
+        // }, 1000);
+        // four.toggleClass('puls');
         // four.css('transform', 'rotate(' + 30 + 'deg)');
         circle.toggleClass('hidden');
     });
 
-///footer
+    ///footer
 
 
 });
