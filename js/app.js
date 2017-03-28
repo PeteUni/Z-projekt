70b2ac32b9d4dd10b6c88db1c752052bf84958b7
PeteUni <peter.r.uni@o2.pl>
2017-03-28 04:09:20 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -31,51 +31,60 @@ $(function() {
         // var head = $('header');
         var section = $('.index');
         var $keyTable = $("<div class='show'><div class='sh1'><p>1</p></div><div class='sh2'><p>2</p></div><div class='sh3'><p>3</p></div><div class='sh4'><p>4</p></div><div class='sh5'><p>5</p></div><div class='sh6'><p>6</p></div><div class='sh7'><p>7</p></div><div class='sh8'><p>8</p></div><div class='sh9'><p>9</p></div></div>");
-        var $tableNr = $("<div class='numb'></div>");
+        var $tableNr = $("<div class='numb'></div><button class='check'>check</button>");
         section.append($keyTable);
         $keyTable.append($tableNr);
-        var buttons = $('.show').children();
+        var buttons = $('.show').children('div');
         console.log(buttons);
 
-        //
-
-
-        buttons.on('click', function() {
-            var html = $(this).find('p').html();
-            console.log(html);
-            // $tableNr.html(html);
-            var liNext = $('<p>');
-            liNext.text(html);
-            $tableNr.append(liNext);
-
-        });
-        // var input = $tableNr.html();
-        // console.log(input);
-
-        //var html = liNext;
         var tab2 = [2, 6, 3, 9];
         var tab1 = [];
-        for (var i = 0; i <= tab2.length; i++) {
+
+        buttons.on('click', function() {
+            var html = parseInt($(this).find('p').html());
+            console.log(html);
+            // $tableNr.html(html);
+            var liNr = $('<p>');
+            liNr.text(html);
+            $tableNr.append(liNr);
+            tab1.push(html);
             console.log(tab1);
-        }
+        });
+        console.log(tab1);
+        console.log(tab2);
+
+        var butt1 = $('.check');
+
+        butt1.on('click', function() {
+            for (var i = 0; i < tab2.length; i++) {
+                if (tab1[i] == tab2[i]) {
+                    console.log('zgodność Kodów');
+
+                }
+            };
+            window.location.href = "./order.html";
+        });
+
     });
 
-var order1 = $('.box1');
-var order2 = $('.box2');
-var order3 = $('.box3');
-var orederMain = $('.order');
+    var order1 = $('.box1');
+    var order2 = $('.box2');
+    var order3 = $('.box3');
+    var orederMain = $('.order');
 
-order1.on('click', function(){
-  var $tableOrd = $("<div class='ord'><p>Order:</p></div>");
-  orederMain.append($tableOrd);
-});
+    order1.on('click', function() {
+        var $tableOrd = $("<div class='ord'><p>Order:</p>Payment: 29,99$<br><button class='btn1'>Buy</button></div>");
+        orederMain.append($tableOrd);
+    });
 
+    order2.on('click', function() {
+        var $tableOrd = $("<div class='ord'><p>Order:</p>Payment: 24,99$<br><button class='btn1'>Buy</button></div>");
+        orederMain.append($tableOrd);
+    });
 
-    // var buttons = $('.show').children();
-    // console.log(buttons);
-    //   buttons.on('click', function(){
-    //   // var p = buttons.find('p').html();
-    //   console.log('dziala');
-    // });
-    //   console.log('dziala');
+    order3.on('click', function() {
+        var $tableOrd = $("<div class='ord'><p>Order:</p>Payment: 27,99$<br><button class='btn1'>Buy</button></div>");
+        orederMain.append($tableOrd);
+    });
+
 });
